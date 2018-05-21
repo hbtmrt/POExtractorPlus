6936a0dc121bfdf85931af6afcd38cd72e9d9146
Haritha Bharatha Tennakoon <hbt.mrt@outlook.com>
2018-05-21 23:42:36 +0530

    Added data to the UI controls

--- POExtractorPlus/POExtractorPlus/Form1.Designer.cs
+++ POExtractorPlus/POExtractorPlus/Form1.Designer.cs
@@ -35,15 +35,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.destinationTextBox = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.poTypesBrowser = new System.Windows.Forms.OpenFileDialog();
-            this.destinationBrowser = new System.Windows.Forms.OpenFileDialog();
             this.allPOFilesListBox = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.rejectedPOFilesListBox = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -92,6 +92,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnBrowingPOFiles);
             // 
             // label3
             // 
@@ -102,13 +103,13 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Destination";
             // 
-            // textBox2
+            // destinationTextBox
             // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(109, 70);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(331, 20);
-            this.textBox2.TabIndex = 6;
+            this.destinationTextBox.Enabled = false;
+            this.destinationTextBox.Location = new System.Drawing.Point(109, 70);
+            this.destinationTextBox.Name = "destinationTextBox";
+            this.destinationTextBox.Size = new System.Drawing.Size(331, 20);
+            this.destinationTextBox.TabIndex = 6;
             // 
             // button2
             // 
@@ -118,6 +119,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Browse";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnBrowsingDestination);
             // 
             // button3
             // 
@@ -128,21 +130,13 @@
             this.button3.Text = "Extract";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // poTypesBrowser
-            // 
-            this.poTypesBrowser.FileName = "openFileDialog1";
-            // 
-            // destinationBrowser
-            // 
-            this.destinationBrowser.FileName = "openFileDialog1";
-            // 
             // allPOFilesListBox
             // 
             this.allPOFilesListBox.FormattingEnabled = true;
             this.allPOFilesListBox.Location = new System.Drawing.Point(109, 146);
             this.allPOFilesListBox.Name = "allPOFilesListBox";
             this.allPOFilesListBox.ScrollAlwaysVisible = true;
-            this.allPOFilesListBox.Size = new System.Drawing.Size(331, 95);
+            this.allPOFilesListBox.Size = new System.Drawing.Size(424, 95);
             this.allPOFilesListBox.TabIndex = 9;
             // 
             // label4
@@ -157,7 +151,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 262);
+            this.label5.Location = new System.Drawing.Point(3, 13);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(92, 13);
             this.label5.TabIndex = 11;
@@ -166,24 +160,33 @@
             // rejectedPOFilesListBox
             // 
             this.rejectedPOFilesListBox.FormattingEnabled = true;
-            this.rejectedPOFilesListBox.Location = new System.Drawing.Point(109, 262);
+            this.rejectedPOFilesListBox.Location = new System.Drawing.Point(97, 13);
             this.rejectedPOFilesListBox.Name = "rejectedPOFilesListBox";
             this.rejectedPOFilesListBox.ScrollAlwaysVisible = true;
-            this.rejectedPOFilesListBox.Size = new System.Drawing.Size(331, 95);
+            this.rejectedPOFilesListBox.Size = new System.Drawing.Size(424, 95);
             this.rejectedPOFilesListBox.TabIndex = 12;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rejectedPOFilesListBox);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(12, 247);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(532, 117);
+            this.panel1.TabIndex = 13;
+            this.panel1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 373);
-            this.Controls.Add(this.rejectedPOFilesListBox);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(547, 251);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.allPOFilesListBox);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.destinationTextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -194,6 +197,8 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PO Extractor Plus";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,15 +212,14 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox destinationTextBox;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.OpenFileDialog poTypesBrowser;
-        private System.Windows.Forms.OpenFileDialog destinationBrowser;
         private System.Windows.Forms.ListBox allPOFilesListBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ListBox rejectedPOFilesListBox;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- POExtractorPlus/POExtractorPlus/Form1.cs
+++ POExtractorPlus/POExtractorPlus/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,56 @@ namespace POExtractorPlus
         public Form1()
         {
             InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            string defaultDestination = Properties.Settings.Default.Destination;
+            this.destinationTextBox.Text = defaultDestination;
+        }
+
+        private void OnBrowsingDestination(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    this.destinationTextBox.Text = fbd.SelectedPath;
+                    Properties.Settings.Default.Destination = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void OnBrowingPOFiles(object sender, EventArgs e)
+        {
+            using (var pfd = new OpenFileDialog())
+            {
+                pfd.Multiselect = true;
+                pfd.Filter = "Pdf Files|*.pdf";
+
+                DialogResult result = pfd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    if (pfd.FileNames.Length > 0) {
+                        SetSelectedFilesToListBox(pfd.FileNames);
+                    }
+                }
+            }
+        }
+
+        private void SetSelectedFilesToListBox(string[] files)
+        {
+            string[] fileNamesOnly = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileNamesOnly[i] = Path.GetFileName(files[0]);
+            }
+
+            this.allPOFilesListBox.DataSource = fileNamesOnly;
         }
     }
 }
